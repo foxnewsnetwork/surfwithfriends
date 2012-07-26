ba63b081a4d6e31eb889281ed6d7dea3b312a6a5
Yuki Nagato <foxnewsnetwork@gmail.com>
2012-07-26 11:31:59 -0700

    added in the forever demon for production purposes

---
+++ forever_production.js
@@ -0,0 +1,11 @@
+// Daemons are for production use
+var forever = require( "forever" );
+
+var child = new (forever.Monitor)('surf.js', {
+    max: 3,
+    silent: true,
+    options: []
+});
+
+child.on('exit', function(){ console.log( "We fucked up"); });
+child.start();
